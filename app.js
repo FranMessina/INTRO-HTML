da7c05e32c4f70aa4f8540db15e0a450bb9f9080
FranMessina <fm230499@gmail.com>
2021-05-15 20:24:36 -0300

    "Subida de logi nregister"

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.listen(3000, () => {
-    console.log("Servidor Corriendo en puerto 3000")
+	console.log("Servidor Corriendo en puerto 3000");
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+	res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/login", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.get("/register", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
